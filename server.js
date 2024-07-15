2603af9eca1423b9f5b03115c726f78316fdc262
agung <agungsur84@gmail.com>
2024-07-15 12:25:53 +0700

    ui input

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'views'));
 
 const db = mysql.createConnection({
